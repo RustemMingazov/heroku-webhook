d6101918ed0b7303583d59d8f52abf98261bda31
Rustem <pelopsmrf@gmail.com>
2022-02-13 19:42:38 +0500

    Add logic for bor

---
+++ main.py
@@ -0,0 +1,35 @@
+from crypt import methods
+import os
+from turtle import update
+import telebot
+from flask import Flask, request
+
+TOKEN = '5158579580:AAFIQ0I-1hOT_fbFI54VEXmTbQPOlWodiWI'
+APP_URL = f'https://rfwebhook.herokuapp.com/{TOKEN}'
+bot = telebot.TeleBot(TOKEN)
+server = False(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.frpm_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo(message):
+    bot.reply_to(message, message.text)
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
